2805ebd37fb0fe8bdd28c35c35026df30756d7c8
r0neel <ronbhagwakar@gmail.com>
2022-02-21 11:27:19 +0000

    add User model

--- models/User.js
+++ models/User.js
@@ -1,2 +1,69 @@
 const { init } = require("../dbConfig");
-const { ObjectId } = require("mongodb");
+
+class User {
+  constructor(data) {
+    this.username = data.username;
+    this.email = data.email;
+    this.hash = data.hash;
+  }
+
+  //get all for testing purposes
+  static get all() {
+    return new Promise(async (res, rej) => {
+      try {
+        const db = await init();
+        let result = db.collections("users").find().toArray();
+        let users = result.map((u) => {
+          new User(u);
+        });
+        res(users);
+      } catch (err) {
+        rej(`Error retrieving users: ${err}`);
+      }
+    });
+  }
+
+  static create({ username, email, password }) {
+    return new Promise(async (res, rej) => {
+      try {
+        const db = await init();
+        let result = db.collections("users").insertOne({
+          username: username,
+          email: email,
+          hash: password
+        });
+        res(result);
+      } catch (err) {
+        rej(`error creating user ${err}`);
+      }
+    });
+  }
+
+  static findByEmail(email) {
+    return new Promise(async (res, rej) => {
+      try {
+        const db = await init();
+        let result = await db.collection("users").find({ email: email });
+        let user = new User(result);
+        res(user);
+      } catch (err) {
+        rej(`Error retrieving user: ${err}`);
+      }
+    });
+  }
+
+  static findByUsername(username) {
+    return new Promise(async (res, rej) => {
+      try {
+        const db = await init();
+        let result = await db.collection("users").find({ username: username });
+        let user = new User(result);
+        res(user);
+      } catch (err) {
+        rej(`Error retrieving user: ${err}`);
+      }
+    });
+  }
+}
+
+module.exports = User;
