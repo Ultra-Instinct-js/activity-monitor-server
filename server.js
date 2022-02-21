21c9fae5a7047090e4eec4e30872103b46a2a4e2
r0neel <ronbhagwakar@gmail.com>
2022-02-21 16:09:04 +0000

    remove habitRoutes from server:

--- server.js
+++ server.js
@@ -7,12 +7,9 @@ server.use(express.json());
 
 const authRoutes = require("./controllers/auth");
 const userRoutes = require("./controllers/users");
-const habitRoutes = require("./controllers/habits");
 
 server.use("/auth", authRoutes);
 server.use("/users", userRoutes);
-server.use("/habits", habitRoutes);
-
 // Root route
 server.get("/", (req, res) => res.send("Hello, client!"));
 
