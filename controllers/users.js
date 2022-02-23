cd34c3a79241d23c46e5c6521150e10faf94a282
r0neel <ronbhagwakar@gmail.com>
2022-02-23 18:07:27 +0000

    fix delete user route

--- controllers/users.js
+++ controllers/users.js
@@ -7,8 +7,7 @@ const { verifyToken } = require("../middleware/verifyUser");
 //delete user
 router.delete("/:id", verifyToken, async (req, res) => {
   try {
-    const user = await User.findByUsername(req.params.id);
-    console.log("found user");
+    const user = await User.findById(req.params.id);
     await user.destroy();
     res.status(204).json("User deleted");
   } catch (err) {
